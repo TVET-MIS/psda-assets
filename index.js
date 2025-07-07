9015396c2f4efd3f36c192317cf5c2b4bcc740e1
maheennasir <maheen.nasir@optimusfox.com>
2025-07-07 18:22:34 +0500

    1.0.25

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import psdaLogo from './assets/svg/logos/psda_logo.svg';
 import psda404 from './assets/svg/imgs/404.svg';
 import psda500 from './assets/svg/imgs/bg_image.svg';
-import bg from './assets/svg/imgs/bg.svg';
+import bg from './assets/svg/imgs/bg_image.svg';
 import enTranslations from './assets/commoni18n/locale/en/translation.json';
 import urTranslations from './assets/commoni18n/locale/ur/translation.json';
 
