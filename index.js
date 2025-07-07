ef0c66d0443dc3fe3b0aa9a671d0663b859574c2
maheennasir <maheen.nasir@optimusfox.com>
2025-07-07 18:59:54 +0500

    added image

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import psdaLogo from './assets/svg/logos/psda_logo.svg';
 import psda404 from './assets/svg/imgs/404.svg';
-import psda500 from './assets/svg/imgs/bg_image.svg';
+import psda500 from './assets/svg/imgs/500.svg';
 import bg from './assets/svg/imgs/bg_image.svg';
 import enTranslations from './assets/commoni18n/locale/en/translation.json';
 import urTranslations from './assets/commoni18n/locale/ur/translation.json';
