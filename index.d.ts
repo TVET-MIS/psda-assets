86569c2c269753d5c21e1bad44ab4dfb107511b4
maheennasir <maheen.nasir@optimusfox.com>
2025-07-07 18:09:10 +0500

    1.0.24

--- index.d.ts
+++ index.d.ts
@@ -5,6 +5,7 @@ declare module '@tvet-mis/psda-assets' {
   export const psdaLogo: string;  // SVG import will return a string (URL)
   export const psda404: string;
   export const psda500: string;
+  export const bg: string;
   export const enTranslations: Record<string, any>;  // JSON will be typed as a generic object
   export const urTranslations: Record<string, any>;
 }
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import psdaLogo from './assets/svg/logos/psda_logo.svg';
 import psda404 from './assets/svg/imgs/404.svg';
-import psda500 from './assets/svg/imgs/500.svg';
+import psda500 from './assets/svg/imgs/bg_image.svg';
+import bg from './assets/svg/imgs/bg.svg';
 import enTranslations from './assets/commoni18n/locale/en/translation.json';
 import urTranslations from './assets/commoni18n/locale/ur/translation.json';
 
 // Export the assets
-export { psdaLogo, psda404, psda500, enTranslations, urTranslations };
+export { psdaLogo, psda404, psda500, bg, enTranslations, urTranslations };
