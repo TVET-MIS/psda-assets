22639af668b13c3c491fde17b39f24e19c218d63
Muhammad-Arsan <muhammad.arsan@optimusfox.com>
2025-07-10 14:15:55 +0500

    1.0.37

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-import psdaLogo from './assets/svg/logos/psda_logo.svg';
-import psda404 from './assets/svg/imgs/404.svg';
-import psda500 from './assets/svg/imgs/500.svg';
-import bg from './assets/svg/imgs/bg_image.svg';
-import enTranslations from './assets/commoni18n/locale/en/translation.json';
-import urTranslations from './assets/commoni18n/locale/ur/translation.json';
-import whatsappIcon from './assets/svg/icons/whatsapp.svg';
+import psdaLogo from "./assets/svg/logos/psda_logo.svg";
+import psda404 from "./assets/svg/imgs/404.svg";
+import psda500 from "./assets/svg/imgs/500.svg";
+import bg from "./assets/svg/imgs/bg_image.svg";
+import userProfile from "./assets/svg/imgs/userprofile.svg";
+import enTranslations from "./assets/commoni18n/locale/en/translation.json";
+import urTranslations from "./assets/commoni18n/locale/ur/translation.json";
+import whatsappIcon from "./assets/svg/icons/whatsapp.svg";
 
 // Export the assets
-export { psdaLogo, psda404,  psda500, bg, whatsappIcon, enTranslations, urTranslations };
+export {
+  psdaLogo,
+  psda404,
+  psda500,
+  bg,
+  whatsappIcon,
+  enTranslations,
+  urTranslations,
+  userProfile,
+};
